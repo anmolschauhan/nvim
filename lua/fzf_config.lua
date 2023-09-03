5936bef27a96e4786459b2b86ceb981072ec2078
Anmol Chauhan <chauhaan@deshaw.com>
2023-09-03 23:50:22 +0530

    Remap

--- lua/fzf_config.lua
+++ lua/fzf_config.lua
@@ -1,11 +1,15 @@
 vim.keymap.set("n", "<c-f>",
+"<cmd>lua require('fzf-lua').grep_curbuf()<CR>", { silent = true })
+
+vim.keymap.set("n", "<Space>f",
 "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
 
-vim.keymap.set("n", "<c-h>",
-"<cmd>lua require('fzf-lua').grep_visual()<CR>", { silent = true })
-
-vim.keymap.set("n", "<c-g>",
+vim.keymap.set("n", "<Space>g",
 "<cmd>lua require('fzf-lua').live_grep_native()<CR>", { silent = true })
 
+vim.keymap.set("n", "<Space>h",
+"<cmd>lua require('fzf-lua').grep_visual()<CR>", { silent = true })
+
+-- We override the map set by lsp-zero.
 vim.keymap.set("n", "gr",
 "<cmd>lua require('fzf-lua').lsp_finder()<CR>", { silent = true })
