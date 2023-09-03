fb901ca6e123c973402a5463703093c609ff2b46
Anmol Chauhan <chauhaan@deshaw.com>
2023-09-03 18:10:52 +0530

    'gr' now use fzf; remaps

--- lua/fzf_config.lua
+++ lua/fzf_config.lua
@@ -1,9 +1,11 @@
-vim.keymap.set("n", "<c-P>",
+vim.keymap.set("n", "<c-f>",
 "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
 
-vim.keymap.set("n", "<c-g>",
+vim.keymap.set("n", "<c-h>",
 "<cmd>lua require('fzf-lua').grep_visual()<CR>", { silent = true })
 
-vim.keymap.set("n", "<c-l>",
+vim.keymap.set("n", "<c-g>",
 "<cmd>lua require('fzf-lua').live_grep_native()<CR>", { silent = true })
 
+vim.keymap.set("n", "gr",
+"<cmd>lua require('fzf-lua').lsp_finder()<CR>", { silent = true })
