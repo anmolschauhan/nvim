f8d1d6d56c3eb67fc98ab10e87a1dd24b9246edf
Anmol Chauhan <chauhaan@deshaw.com>
2023-09-04 16:48:59 +0530

    Added ensured treesitter grammars

--- lua/treesitter_config.lua
+++ lua/treesitter_config.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  
+    ensure_installed = {'c', 'cpp', 'python', 'lua', 'make', 'bash'},
+
     highlight = {
       enable = true,
       -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
